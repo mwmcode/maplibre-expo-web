7218dabbfed9609b6a67da7d79e84c8945b314af
M M <mm@Ms-MacBook-Pro-2.local>
2025-01-27 22:41:51 -0500

    web browser works

--- app/index.tsx
+++ app/index.tsx
@@ -1,67 +1,28 @@
 import { useState } from 'react';
+import { Button, Text, View, StyleSheet } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
-import {
-  exchangeCodeAsync,
-  makeRedirectUri,
-  useAuthRequest,
-  useAutoDiscovery,
-} from 'expo-auth-session';
-import { Button, Text, SafeAreaView } from 'react-native';
-import Env from '../env.json';
-import { ErrorBoundary } from '@/ErrBoundary';
-
-WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
-  // Endpoint
-  const discovery = useAutoDiscovery(Env.AAD_CONFIG.issuer);
-  const redirectUri = makeRedirectUri({
-    scheme: Env.AAD_CONFIG.schema,
-    path: Env.AAD_CONFIG.redirectUrl,
-  });
-  const clientId = Env.AAD_CONFIG.clientId;
+  const [result, setResult] = useState(null);
 
-  // We store the JWT in here
-  const [token, setToken] = useState<string | null>(null);
-
-  // Request
-  const [request, , promptAsync] = useAuthRequest(
-    {
-      clientId,
-      scopes: ['openid', 'profile', 'email', 'offline_access'],
-      redirectUri,
-    },
-    discovery,
-  );
-
+  const _handlePressButtonAsync = async () => {
+    let result = await WebBrowser.openBrowserAsync('https://expo.dev');
+    setResult(result);
+  };
   return (
-    <ErrorBoundary>
-      <SafeAreaView>
-        <Button
-          disabled={!request}
-          title="Login"
-          onPress={() => {
-            promptAsync().then(codeResponse => {
-              if (request && codeResponse?.type === 'success' && discovery) {
-                exchangeCodeAsync(
-                  {
-                    clientId,
-                    code: codeResponse.params.code,
-                    extraParams: request.codeVerifier
-                      ? { code_verifier: request.codeVerifier }
-                      : undefined,
-                    redirectUri,
-                  },
-                  discovery,
-                ).then(res => {
-                  setToken(res.accessToken);
-                });
-              }
-            });
-          }}
-        />
-        <Text>{token}</Text>
-      </SafeAreaView>
-    </ErrorBoundary>
+    <View style={styles.container}>
+      <Button title="Open WebBrowser" onPress={_handlePressButtonAsync} />
+      <Text>{result && JSON.stringify(result)}</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 30,
+    backgroundColor: '#ecf0f1',
+  },
+});
